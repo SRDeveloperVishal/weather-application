641e65d7bce12465be9c865d450f252311266e8a
Shubham Karala <shubhamkarala@gmail.com>
2018-01-27 12:15:44 +0530

    final scheduling

--- clock.py
+++ clock.py
@@ -3,8 +3,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=10)
+@sched.scheduled_job('interval', minutes=1)
 def timed_job():
-    os.system('python worker.py')
+    print('This job is run every three minutes.')
 
 sched.start()
